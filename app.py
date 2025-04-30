ea70a97a703984a9f523f4b36ddbad3e94fc9156
pim905 <pimwerkman@gmail.com>
2025-04-30 12:37:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,10 +13,14 @@ nltk_data_path = os.path.join(os.getcwd(), "nltk_data")
 nltk.data.path.append(nltk_data_path)
 
 def download_nltk_data():
-    nltk.download('punkt', download_dir=nltk_data_path)
-    nltk.download('maxent_ne_chunker', download_dir=nltk_data_path)
-    nltk.download('words', download_dir=nltk_data_path)
-    nltk.download('averaged_perceptron_tagger', download_dir=nltk_data_path)
+    try:
+        nltk.download('punkt', download_dir=nltk_data_path)  # Tokenizer models
+        nltk.download('punkt_tab', download_dir=nltk_data_path)  # Needed for sentence tokenization
+        nltk.download('maxent_ne_chunker', download_dir=nltk_data_path)
+        nltk.download('words', download_dir=nltk_data_path)
+        nltk.download('averaged_perceptron_tagger', download_dir=nltk_data_path)
+    except Exception as e:
+        st.error(f"Error downloading NLTK data: {e}")
 
 download_nltk_data()
 
